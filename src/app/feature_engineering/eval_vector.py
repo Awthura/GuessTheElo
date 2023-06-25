83da8e74492e0b4face4e269cfcef442c7ac35ad
Awthura <awthura2016@gmail.com>
2023-06-25 16:25:52 +0200

    material imbalanve

---
+++ src/app/feature_engineering/eval_vector.py
@@ -0,0 +1,47 @@
+import chess
+import chess.pgn
+import chess.engine
+import json
+import re 
+
+# Path to Stockfish engine executable
+stockfish_path = 'Stockfish/stockfish_15.1_win_x64_avx2/stockfish-windows-2022-x86-64-avx2.exe'
+
+# Function to evaluate the position using Stockfish
+def evaluate_position(position):
+    engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
+    
+    # Evaluate the position using Stockfish
+    analysis = engine.analyse(position, chess.engine.Limit(depth=20))
+    score = analysis["score"].relative.score()
+
+    engine.quit()
+
+    return score
+
+# Load the preprocess.json file
+with open('src/data/tinysample.json', 'r') as file:
+    data = json.load(file)
+
+# Iterate over the games and calculate evaluation values after each move
+for entry in data:
+    moves_san = entry['moves_san'].split()
+
+    evaluations = []  # List to store evaluations after each move
+
+    # Create a chess board and play the moves on it
+    board = chess.Board()
+    for move in moves_san:
+        board.push_san(move)
+
+        # Evaluate the position after each move using Stockfish
+        evaluation = evaluate_position(board)
+        evaluations.append(evaluation)
+
+    # Update the entry with the evaluation values after each move
+    entry['evaluations'] = evaluations
+    print("Success")
+
+# Save the updated data to a new file feature_engineered.json
+with open('src/data/fetiny.json', 'w') as file:
+    json.dump(data, file, indent=4)
